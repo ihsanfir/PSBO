116914c2e74eef20dd0755be75d24ff03acfe9fc
Dwiki <dwikiramdhani53@gmail.com>
2021-06-13 12:23:43 +0700

    add dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../../components/Navbar';
+import MainContent from '../../components/MainContent';
 
 export default function Home() {
     return (
         <Navbar>
-
+            <MainContent />
         </Navbar>
     );
 };
