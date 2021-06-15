9a2ed349353c17c830d40c8c0152310fa029feb5
Ihsan Firdaus <53510551+ihsanfir@users.noreply.github.com>
2021-06-16 00:27:17 +0700

    finish add link at admin

--- pages/admin/meet.js
+++ pages/admin/meet.js
@@ -10,25 +10,25 @@ export async function getServerSideProps(context) {
     Nama: "Admin"
   }
   // fetch all data jadwal
-  // const res = await axios.get(
-  //   process.env.NEXT_PUBLIC_BACKEND_URL + "/jadwal",
-  //   {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: "Bearer" + process.env.NEXT_PUBLIC_TOKEN_ADMIN,
-  //     },
-  //   }
-  // );
-  // const schedule = await res.data;
+  const res = await axios.get(
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/jadwal",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+  const schedule = await res.data;
   return {
-    props: { user, token }, // Will be passed to the page component as props
+    props: { user, token, schedule }, // Will be passed to the page component as props
   };
 }
 
-export default function Meet({ user, token }) {
+export default function Meet({ user, token, schedule }) {
   return (
     <Navbar user={user} token={token} isAdmin={true}>
-      <VidconForm />
+      <VidconForm schedule={schedule} token={token} />
     </Navbar>
   );
 }
@@ -36,4 +36,5 @@ export default function Meet({ user, token }) {
 Meet.propTypes = {
   user: PropTypes.object,
   token: PropTypes.string,
+  schedule: PropTypes.object,
 };
